d375868e24a695fc175b566f34dbb90c9bde2b1d
drmarduk <dr.marduk@gmail.com>
2014-08-06 13:32:03 +0200

    scrollhack

--- html.go
+++ html.go
@@ -1,11 +1,11 @@
 package main
 
-var HtmlLogHead string = `<!DOCTYPE html><html><head><meta http-equiv="refresh" content="5"></head><body>`
-var HtmlLogEnd string = "</body></html>"
+var HtmlLogHead string = `<!DOCTYPE html><html><head><meta http-equiv="refresh" content="5;URL='/log#scrollhack'"></head><body>`
+var HtmlLogEnd string = "<a name='scrollhack'>&nbsp;</a></body></html>"
 
 var HtmlMain string = `<!DOCTYPE html>
 <html><title>Doclol Irc Proxy</title></head><body>
-<iframe src="./log" height="100px"><p>Deine Admins suck0rn.</p></iframe>
+<iframe src="./log#scrollhack" height="100px"><p>Deine Admins suck0rn.</p></iframe>
 <form action="/" method="post">
 Nachricht: <input type="text" name="content">
 <input type="submit">

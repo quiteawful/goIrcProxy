c9f6827c6b50369dfcd654cf0c8c0129961e6926
drmarduk <dr.marduk@gmail.com>
2014-08-06 13:55:36 +0200

    escape htmlfoo.

--- messagelog.go
+++ messagelog.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html"
 	"time"
 )
 
@@ -26,6 +27,7 @@ func (l *Log) AddWebLog(user, content string) {
 
 func (l *Log) AddIrcLog(user, content string) {
 	l.checkLimit()
+	content = html.EscapeString(content)
 	l.MessageLog = append(l.MessageLog, NewMessage(user, content))
 }
 
